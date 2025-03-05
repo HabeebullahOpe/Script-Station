8c01fa7a3429f1eeaf0b8d31081f0bdfd4b7a3a4
Beeb <opeyemihabeebullah279@gmail.com>
2025-03-06 00:03:48 +0100

    Add just checking

---
+++ just-checking/target/debug/deps/just_checking-09a03cdd1a9d7e8a.d
@@ -0,0 +1,5 @@
+C:\Users\hp\projects\Script-Station\just-checking\target\debug\deps\libjust_checking-09a03cdd1a9d7e8a.rmeta: src\main.rs
+
+C:\Users\hp\projects\Script-Station\just-checking\target\debug\deps\just_checking-09a03cdd1a9d7e8a.d: src\main.rs
+
+src\main.rs:
---
+++ just-checking/target/debug/deps/just_checking-ee3bdf87f4274f9a.d
@@ -0,0 +1,5 @@
+C:\Users\hp\projects\Script-Station\just-checking\target\debug\deps\libjust_checking-ee3bdf87f4274f9a.rmeta: src\main.rs
+
+C:\Users\hp\projects\Script-Station\just-checking\target\debug\deps\just_checking-ee3bdf87f4274f9a.d: src\main.rs
+
+src\main.rs:
